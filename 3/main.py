31c4e4494cd7e82abfed9f2f48c4e2b7057a9350
vicktor301211 <iva301211@gmail.com>
2024-10-27 18:11:34 +0300

    изменения в коде

--- 3/main.py
+++ 3/main.py
@@ -5,10 +5,22 @@ KEY_S = 83
 KEY_A = 65
 KEY_D = 68
 KEY_F = 70
+# KEY_RIGHT = 39
+# KEY_LEFT = 37
+# KEY_UP = 38
+# KEY_DOWN = 40
+FPS = 90
+CANS = 15
+clicks = 0
 
+def update():
+    player.update()
+    check_collision()
+    w.after(1000//FPS, update)
 def check_collision():
     if player.intersects(enemy):
-        print('Столкновение танков')
+        #print('Столкновение танков')
+        player.undo_move()
 def key_press(event):
     if event.keycode == KEY_W:
         player.forward()
@@ -18,14 +30,33 @@ def key_press(event):
         player.left()
     if event.keycode == KEY_D:
         player.right()
+    # if event.keycode == KEY_UP:
+    #     enemy.forward()
+    # if event.keycode == KEY_DOWN:
+    #     enemy.backward()
+    # if event.keycode == KEY_LEFT:
+    #     enemy.left()
+    # if event.keycode == KEY_RIGHT:
+    #     enemy.right()
     if event.keycode == KEY_F:
-        player.__fuel = 100
+        global CANS, clicks
+        CANS-=1
+        if CANS > 0:
+            player.refuel()
+            print(CANS, 'канистр')
+        else:
+            clicks += 1
+            print('Нет канистр c бензином')
+            if clicks >= 10:
+                print('ЕСЛИ ТЫ ПРОДОЛЖИШЬ, ИГРА СЛОМАЕТСЯ')
+        # enemy.refuel()
     check_collision()
 w = Tk()
 w.title('Танки на минималках 2.0')
 canv = Canvas(w, width=800, height=600, bg = 'alice blue')
 canv.pack()
-player = Tank(canvas=canv, x=100, y=50, ammo=100)
-enemy = Tank(canvas=canv, x=300, y=300, ammo=100)
+player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1)
+enemy = Tank(canvas=canv, x=300, y=300, ammo=100, speed=1)
 w.bind('<KeyPress>', key_press)
+update()
 w.mainloop()
--- 3/tank.py
+++ 3/tank.py
@@ -1,31 +1,35 @@
 from tkinter import PhotoImage
-
 from hitbox import Hitbox
 class Tank:
     __count = 0
-    __SIZE = 100
+    #__SIZE = 85
     def __init__(self,canvas,x,y,ammo = 100, model = 'T - 14 Армата',speed = 10, file_up = '../img/tank_up.png', file_down = '../img/tank_down.png',
                  file_left = '../img/tank_left.png', file_right = '../img/tank_right.png'):
         self.__skin_up = PhotoImage(file=file_up)
         self.__skin_down = PhotoImage(file=file_down)
         self.__skin_left = PhotoImage(file=file_left)
         self.__skin_right = PhotoImage(file=file_right)
-        self.__hitbox = Hitbox(x, y, Tank.__SIZE, Tank.__SIZE)
+        self.__hitbox = Hitbox(x, y, self.get_size(), self.get_size())
         self.__canvas = canvas
         Tank.__count+=1
         self.__model = model #моедль
-        self.__fuel = 1000000
+        self.__fuel = 1000
         self.__speed = speed
         self.__hp = 100 #здоровье
         self.__xp = 0 #опыт
         self.__ammo = ammo #боекомплект
         self.__x = x#положение по оси х
         self.__y = y #положение по оси у
+        self.__vx = 0
+        self.__vy = 0
+        self.__dx = 0
+        self.__dy = 0
         if self.__x < 0:
             self.__x=0
         if self.__y < 0:
             self.__y=0
         self.__create()
+        self.right()
     def fire(self):
         if self.__ammo > 0:
             self.__ammo-=1
@@ -33,36 +37,40 @@ class Tank:
         else:
             print('Нет снарядов')
     def backward(self):
-        if self.__fuel>0:
-            self.__y+=self.__speed
-            self.__update_hitbox()
-            self.__repaint()
-            self.__fuel-=1
-            self.__canvas.itemconfig(self.id, image=self.__skin_down)
-            self.__repaint()
+        self.__vx = 0
+        self.__vy = 1
+        self.__canvas.itemconfig(self.id, image=self.__skin_down)
     def forward(self):
-        if self.__fuel>0:
-            self.__y+=-self.__speed
-            self.__update_hitbox()
-            self.__repaint()
-            self.__fuel-=1
-            self.__canvas.itemconfig(self.id, image=self.__skin_up)
-            self.__repaint()
+        self.__vx = 0
+        self.__vy = -1
+        self.__canvas.itemconfig(self.id, image=self.__skin_up)
     def left(self):
-        if self.__fuel>0:
-            self.__x+=-self.__speed
-            self.__update_hitbox()
-            self.__repaint()
-            self.__fuel-=1
-            self.__canvas.itemconfig(self.id, image=self.__skin_left)
-            self.__repaint()
+        self.__vx = -1
+        self.__vy = 0
+        self.__canvas.itemconfig(self.id, image=self.__skin_left)
     def right(self):
-        if self.__fuel>0:
-            self.__x+=self.__speed
-            self.__fuel-=1
+        self.__vx = 1
+        self.__vy = 0
+        self.__canvas.itemconfig(self.id, image=self.__skin_right)
+    def refuel(self):
+            self.__fuel=1000
+    def update(self):
+        if self.__fuel > self.__speed:
+            # self.__x += self.__vx * self.__speed
+            # self.__y += self.__vy * self.__speed
+            self.__dx = self.__vx * self.__speed
+            self.__dy = self.__vy * self.__speed
+            self.__x += self.__dx
+            self.__y += self.__dy
+            self.__fuel-= self.__speed
             self.__update_hitbox()
-            self.__canvas.itemconfig(self.id, image=self.__skin_right)
             self.__repaint()
+    def undo_move(self):
+        self.__x -= self.__dx
+        self.__y -= self.__dy
+        self.__fuel += self.__speed
+        self.__update_hitbox()
+        self.__repaint()
     def __create(self):
         self.id = self.__canvas.create_image(self.__x, self.__y, image = self.__skin_up, anchor='nw')
     def __repaint(self):
@@ -90,9 +98,9 @@ class Tank:
     @staticmethod
     def get_count():
         return Tank.__count
-    @staticmethod
-    def get_size():
-        return Tank.__SIZE
+    #@staticmethod
+    def get_size(self):
+        return self.__skin_up.width()
     def __str__(self):
         return (f'Модель: {self.__model}, Здоровье: {self.__hp},Топливо: {self.__fuel}, '
                      f'Опыт: {self.__xp}, Боекомплет: {self.__ammo}, '
